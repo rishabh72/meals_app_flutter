57a2ab20726810dab31ece20759c24c671137abd
rishabh <sonirishabh018@gmail.com>
2022-01-07 11:54:26 +0530

    linear gradients

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_complete_guide/category_item.dart';
+import './dummy-data.dart';
 
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GridView(
-      children: [],
+      children: DUMMY_CATEGORIES.map((cat) {
+        return CategoryItem(cat.title, cat.color);
+      }).toList(),
       gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
         maxCrossAxisExtent: 200,
         childAspectRatio: 3 / 2,
